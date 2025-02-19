56c712f91ded8ff978bedb8fd7e48cacc9765b60
Hoffi120186 <marc.hoffmann86@gmx.de>
2025-02-19 17:01:03 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -12,6 +12,7 @@ const urlsToCache = [
   '/patient9.html', '/patient10.html', '/patient11.html', '/patient12.html',
   '/patient13.html', '/patient14.html', '/patient15.html', '/patient16.html',
   '/patient17.html', '/patient18.html', '/patient19.html', '/patient20.html'
+  '/Status4.html'
 ];
 
 // Installieren und Caching der Dateien
